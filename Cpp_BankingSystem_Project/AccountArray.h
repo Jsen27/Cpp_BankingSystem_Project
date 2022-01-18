4a46fcb2869bc7853018c30e31bff7daf959cb9d
sehoon27@naver.com <sehoon27@naver.com>
2022-01-18 19:31:28 +0900

    0.8버젼, 복습 필요할듯

--- Cpp_BankingSystem_Project/AccountArray.h
+++ Cpp_BankingSystem_Project/AccountArray.h
@@ -1,13 +1,20 @@
 #pragma once
 #include "Account.h"
+
 typedef Account* ACCOUNT_PTR;
 
 class BoundCheckAccountPtrArray
 {
 private:
-	ACCOUNT_PTR* ptr;
+	ACCOUNT_PTR* arr;
 	int arrlen;
+	BoundCheckAccountPtrArray(BoundCheckAccountPtrArray&) {}
+	BoundCheckAccountPtrArray& operator=(BoundCheckAccountPtrArray&) {}
+
 public:
 	BoundCheckAccountPtrArray(int len = 100);
-	
+	ACCOUNT_PTR& operator[](int idx);
+	ACCOUNT_PTR& operator[](int idx)const;
+	int GetArrLen()const;
+	~BoundCheckAccountPtrArray();
 };
